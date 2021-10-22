f62912ff64f5bdac6ca3306ead8dbf15afa7eee0
Emanuel-David Bratasanu <davidthearchetype@gmail.com>
2021-10-22 16:07:54 +0100

    Added base Welcome component, needs reediting, co-author @Mitchel, mitch2103@hotmail.co.uk

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Text } from "@chakra-ui/react";
+
+export function Welcome({ name = "there" }) {
+  let val = " - Fill out personal details.";
+  return (
+    <section>
+      <Text className="font-semibold">
+        Hello {name}! Welcome to your School of Code application, our
+        application has several steps:
+      </Text>{" "}
+      <div className="flex">
+        <Text className="font-semibold">Stage 1 </Text>
+        <Text>{val}</Text>
+      </div>{" "}
+      <div className="flex">
+        <Text className="font-semibold">Stage 2 </Text>- Create a pixel
+        character.
+      </div>{" "}
+      <div className="flex">
+        <Text className="font-semibold">Stage 3 </Text>- Record a video
+        interview.
+      </div>{" "}
+      <div className="flex">
+        <Text className="font-semibold">Stage 4 </Text>- Create a game with
+        Scratch.
+      </div>{" "}
+      <Text className="font-semibold">
+        More details of each stage will be given when you reach the stage. You
+        have 4 weeks from now on to complete Stages 1 to 4 - each new stage will
+        unlock when you complete the current stage.
+      </Text>
+    </section>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { ChakraProvider, Heading } from "@chakra-ui/react";
 import { MainButton } from "../components/MainButton";
 import { Footer } from "../components/Footer";
+import { Welcome } from "../components/Welcome";
 
 // markup
 const IndexPage = () => {
@@ -10,6 +11,7 @@ const IndexPage = () => {
       <main>
         <Heading>I'm a Heading</Heading>
         <MainButton onClick={() => alert("Wow")} />
+        <Welcome />
       </main>
       <Footer />
     </ChakraProvider>
