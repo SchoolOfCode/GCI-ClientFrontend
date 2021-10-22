b2ad1ac270979aa983a3d7aca40c4487b11f9fef
tzuyunlin <tzuyunlin.tw@gmail.com>
2021-10-22 14:57:20 +0100

    contactform component added
    Co-authored-by: @Tommosaurus

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import Wrapper from "../components/Wrapper";
+import ContactForm from "../components/ContactForm";
 
 // markup
 const IndexPage = () => {
   return (
     <ChakraProvider>
       <main></main>
-
     </ChakraProvider>
   );
 };
