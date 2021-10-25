67659eb890c675eee7c937cf38be1c0f78b781bd
Emanuel-David Bratasanu <davidthearchetype@gmail.com>
2021-10-25 16:38:16 +0100

    Added Stage 2 intro, need to finish it. Corrected GenericInput, co-author @Colsxn

--- src/components/StageTwo/index.js
+++ src/components/StageTwo/index.js
@@ -5,6 +5,26 @@ import GenericInput from "../GenericInput";
 export function StageTwo() {
   return (
     <section id="stage2section">
+      <h4>
+        This isn't your standard application process because this isn't your
+        standard course!
+      </h4>
+      <h4>
+        This isn't your standard application process because this isn't your
+        standard course!
+      </h4>
+      <h4>
+        This isn't your standard application process because this isn't your
+        standard course!
+      </h4>
+      <h4>
+        This isn't your standard application process because this isn't your
+        standard course!
+      </h4>
+      <h4>
+        This isn't your standard application process because this isn't your
+        standard course!
+      </h4>
       <GenericInput label="Character link here please..." role="stage2text1" />
       <MainButton
         buttonText="To Stage 3"
@@ -13,6 +33,7 @@ export function StageTwo() {
             document.querySelector(".stage2text1").value +
               " has been sent to the cloud!"
           );
+          document.querySelector(".stage3").click();
         }}
       />
     </section>
