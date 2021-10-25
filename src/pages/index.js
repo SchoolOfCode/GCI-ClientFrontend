35f179652a38ce305a728ccbe6d69003f3b10659
Colsxn <mitch2103@hotmail.co.uk>
2021-10-25 10:09:37 +0100

    nav side bar added co-author @DaveArchetype

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,57 @@ import * as React from "react";
 import { ChakraProvider, Heading } from "@chakra-ui/react";
 import { MainButton } from "../components/MainButton";
 import { Footer } from "../components/Footer";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 
 // markup
 const IndexPage = () => {
   return (
     <ChakraProvider>
       <main>
-        <Heading>I'm a Heading</Heading>
-        <MainButton onClick={() => alert("Wow")} />
+        <Tabs
+          variant="solid-rounded"
+          align="center"
+          orientation="vertical"
+          isLazy
+        >
+          <TabList>
+            <Tab>Welcome</Tab>
+            <Tab>Stage 1</Tab>
+            <Tab>Stage 2</Tab>
+            <Tab>Stage 3</Tab>
+            <Tab>Stage 4</Tab>
+            <Tab>Interview</Tab>
+            <Tab>Result</Tab>
+            <Tab>F.A.Q</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <Heading>I'm a Heading</Heading>
+            </TabPanel>
+            <TabPanel>
+              <MainButton onClick={() => alert("Wow")} />
+            </TabPanel>
+            <TabPanel>
+              <p>Stage 2!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Stage 3!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Stage 4!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Interview!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Result!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>FAQ!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </main>
       <Footer />
     </ChakraProvider>
