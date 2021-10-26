4d6fdbfeb5a1f63271ebc7cb42bad66ffc80cc64
Emanuel-David Bratasanu <davidthearchetype@gmail.com>
2021-10-26 10:05:04 +0100

    Adding auth to our version, co-author @Colsxn

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import * as React from "react";
-// import Amplify, { Auth } from "aws-amplify";
-// import awsconfig from "../aws-exports";
-// import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import Amplify from "aws-amplify";
+import awsconfig from "../aws-exports";
 import {
   ChakraProvider,
   Heading,
@@ -11,6 +10,11 @@ import {
   Tab,
   TabPanel,
 } from "@chakra-ui/react";
+import {
+  AmplifySignOut,
+  AmplifyAuthenticator,
+  AmplifySignIn,
+} from "@aws-amplify/ui-react";
 import Header from "../components/Header";
 import ContactForm from "../components/ContactForm";
 import { MainButton } from "../components/MainButton";
@@ -18,121 +22,153 @@ import { Footer } from "../components/Footer";
 import { Welcome } from "../components/Welcome";
 import { StageTwo } from "../components/StageTwo";
 import "./index.css";
-// Amplify.configure(awsconfig);
+Amplify.configure(awsconfig);
 
 // markup
 const IndexPage = () => {
   return (
     <ChakraProvider>
-      {/* <AmplifySignOut /> */}
-      <main className="wrapper">
-        <Header />
-        <Tabs
-          className="m-20"
-          variant="line"
-          align="left"
-          orientation="vertical"
-          isLazy
+      <AmplifyAuthenticator>
+        <div
+          className="App"
+          slot="sign-in"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            height: "100vh",
+            width: "100%",
+            backgroundSize: "1800px 1800px",
+            marginBottom: "20px",
+          }}
         >
-          <TabList>
-            <Tab
-              className="border-2 border-white text-white font-semibold"
-              bg="#8896A3"
-              _selected={{ color: "white", bg: "#4A90E2" }}
-            >
-              <p className="welcome">Welcome</p>
-            </Tab>
-            <Tab
-              className="border-2 border-white text-white font-semibold"
-              bg="#8896A3"
-              _selected={{ color: "white", bg: "#4A90E2" }}
-            >
-              <p className="stage1">Stage 1</p>
-            </Tab>
-            <Tab
-              className="border-2 border-white text-white font-semibold"
-              bg="#8896A3"
-              _selected={{ color: "white", bg: "#4A90E2" }}
-            >
-              <p className="stage2">Stage 2</p>
-            </Tab>
-            <Tab
-              className="border-2 border-white text-white font-semibold"
-              bg="#8896A3"
-              _selected={{ color: "white", bg: "#4A90E2" }}
-            >
-              <p className="stage3">Stage 3</p>
-            </Tab>
-            <Tab
-              className="border-2 border-white text-white font-semibold"
-              bg="#8896A3"
-              _selected={{ color: "white", bg: "#4A90E2" }}
-            >
-              <p className="stage4">Stage 4</p>
-            </Tab>
-            <Tab
-              className="border-2 border-white text-white font-semibold"
-              bg="#8896A3"
-              _selected={{ color: "white", bg: "#4A90E2" }}
-            >
-              <p className="interview">Interview</p>
-            </Tab>
-            <Tab
-              className="border-2 border-white text-white font-semibold"
-              bg="#8896A3"
-              _selected={{ color: "white", bg: "#4A90E2" }}
-            >
-              <p className="result">Result</p>
-            </Tab>
-            <Tab
-              className="border-2 border-white text-white font-semibold"
-              bg="#8896A3"
-              _selected={{ color: "white", bg: "#4A90E2" }}
-            >
-              F.A.Q
-            </Tab>
-          </TabList>
+          <Header />
+          <AmplifySignIn headerText="Sign in to access your SoC application" />
+        </div>
+        <main className="wrapper">
+          <Header />
+          <Tabs
+            className="m-20"
+            variant="line"
+            align="left"
+            orientation="vertical"
+            isLazy
+          >
+            <TabList>
+              <Tab
+                className="border-2 border-white text-white font-semibold"
+                bg="#8896A3"
+                _selected={{ color: "white", bg: "#4A90E2" }}
+              >
+                <p className="welcome">Welcome</p>
+              </Tab>
+              <Tab
+                className="border-2 border-white text-white font-semibold"
+                bg="#8896A3"
+                _selected={{ color: "white", bg: "#4A90E2" }}
+              >
+                <p className="stage1">Stage 1</p>
+              </Tab>
+              <Tab
+                className="border-2 border-white text-white font-semibold"
+                bg="#8896A3"
+                _selected={{ color: "white", bg: "#4A90E2" }}
+              >
+                <p className="stage2">Stage 2</p>
+              </Tab>
+              <Tab
+                className="border-2 border-white text-white font-semibold"
+                bg="#8896A3"
+                _selected={{ color: "white", bg: "#4A90E2" }}
+              >
+                <p className="stage3">Stage 3</p>
+              </Tab>
+              <Tab
+                className="border-2 border-white text-white font-semibold"
+                bg="#8896A3"
+                _selected={{ color: "white", bg: "#4A90E2" }}
+              >
+                <p className="stage4">Stage 4</p>
+              </Tab>
+              <Tab
+                className="border-2 border-white text-white font-semibold"
+                bg="#8896A3"
+                _selected={{ color: "white", bg: "#4A90E2" }}
+              >
+                <p className="interview">Interview</p>
+              </Tab>
+              <Tab
+                className="border-2 border-white text-white font-semibold"
+                bg="#8896A3"
+                _selected={{ color: "white", bg: "#4A90E2" }}
+              >
+                <p className="result">Result</p>
+              </Tab>
+              <Tab
+                className="border-2 border-white text-white font-semibold"
+                bg="#8896A3"
+                _selected={{ color: "white", bg: "#4A90E2" }}
+              >
+                Pre-Course
+              </Tab>
+              <Tab
+                className="border-2 border-white text-white font-semibold"
+                bg="#8896A3"
+                _selected={{ color: "white", bg: "#4A90E2" }}
+              >
+                F.A.Q
+              </Tab>
+              <Tab
+                className="border-2 border-white text-white font-semibold"
+                bg="#8896A3"
+                _selected={{ color: "white", bg: "#4A90E2" }}
+              >
+                Sign out
+              </Tab>
+            </TabList>
 
-          <TabPanels>
-            <TabPanel>
-              <Welcome />
-            </TabPanel>
-            <TabPanel>
-              <Header text="Stage 1"></Header>
-              <MainButton onClick={() => alert("Wow")} />
-            </TabPanel>
-            <TabPanel>
-              <StageTwo />
-            </TabPanel>
-            <TabPanel>
-              <Header text="Stage 3"></Header>
-            </TabPanel>
-            <TabPanel>
-              <Header text="Stage 4"></Header>
-            </TabPanel>
-            <TabPanel>
-              <Header text="Interview"></Header>
-            </TabPanel>
-            <TabPanel>
-              <Header text="Result"></Header>
-            </TabPanel>
-            <TabPanel>
-              <Header text="Result"></Header>
-              <Header text="Result"></Header>
-              <Header text="Result"></Header>
-              <Header text="Result"></Header>
-              <Header text="Result"></Header>
-              <Heading>Frequently asked questions</Heading>
-
-              <ContactForm />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </main>
-      <Footer />
+            <TabPanels>
+              <TabPanel>
+                <Welcome />
+              </TabPanel>
+              <TabPanel>
+                <Header text="Stage 1"></Header>
+                <MainButton onClick={() => alert("Wow")} />
+              </TabPanel>
+              <TabPanel>
+                <StageTwo />
+              </TabPanel>
+              <TabPanel>
+                <Header text="Stage 3"></Header>
+              </TabPanel>
+              <TabPanel>
+                <Header text="Stage 4"></Header>
+              </TabPanel>
+              <TabPanel>
+                <Header text="Interview"></Header>
+              </TabPanel>
+              <TabPanel>
+                <Header text="Result"></Header>
+              </TabPanel>
+              <TabPanel>
+                <Heading>Frequently asked questions</Heading>
+                <ContactForm />
+              </TabPanel>
+              <TabPanel>
+                <Heading>Frequently asked questions</Heading>
+              </TabPanel>
+              <TabPanel>
+                <AmplifySignOut />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </main>
+        <Footer />
+      </AmplifyAuthenticator>
     </ChakraProvider>
   );
 };
 
-// export default withAuthenticator(IndexPage);
+//
 export default IndexPage;
