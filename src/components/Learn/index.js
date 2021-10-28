4243f652c22ba1bad79648222292f426fb09d6c3
Tommosaurus <87076987+Tommosaurus@users.noreply.github.com>
2021-10-28 13:07:18 +0100

    wrapped fetch request in a useEffect

--- src/components/Learn/index.js
+++ src/components/Learn/index.js
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { render } from "react-dom";
+
 import Markdown from "markdown-to-jsx";
 import "./learn.css";
-const marked = require("marked");
 
 export function Learn() {
   const [text, setText] = useState("");
-  fetch(
-    "https://raw.githubusercontent.com/SchoolOfCode/bootcamp-pre-course/main/README.md"
-  )
-    .then((response) => response.text())
-    .then((result) => {
-      setText(result);
-    });
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/SchoolOfCode/bootcamp-pre-course/main/README.md"
+    )
+      .then((response) => response.text())
+      .then((result) => {
+        setText(result);
+      });
+  });
+
   return (
     <Markdown
       options={{
@@ -47,7 +50,6 @@ export function Learn() {
               className: "list-disc ml-5",
             },
           },
-          
         },
       }}
     >
