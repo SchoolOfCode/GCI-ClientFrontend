9e2b6e7af7005a59effcd0b8d7a436ef1e17d1b7
Colsxn <mitch2103@hotmail.co.uk>
2021-10-29 14:10:41 +0100

    added function to save the answers from stage 2,3,4
    Co-authored-by: @DaveArchetype
    Co-authored-by: @Tommosaurus
    Co-authored-by: @lintzuyun

--- src/components/StageFour/index.js
+++ src/components/StageFour/index.js
@@ -11,15 +11,24 @@ import GenericInput from "../GenericInput";
 import { MainButton } from "../MainButton";
 import PrecourseText from "../PrecourseText";
 
-export default function StageFour() {
+export default function StageFour({ userId }) {
   const [render, setRender] = useState(false);
 
   function handleClick() {
-    console.log(
-      "this is the value",
-      document.querySelector(".stage4question1").value
-    );
-    setRender(true);
+    let answer = document.querySelector(".stage4question1").value;
+
+    fetch(`${process.env.API_URL}/users/${userId}`, {
+      method: "PATCH",
+      mode: "no-cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(answer),
+    }).then(setRender(true));
 
     return;
   }
