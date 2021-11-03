ce348fc0cf81a2caf53e94996978bf69f68de914
Emanuel-David Bratasanu <davidthearchetype@gmail.com>
2021-11-03 12:35:23 +0000

    Fixed login

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Amplify from "aws-amplify";
+import Amplify, { Hub, Logger } from "aws-amplify";
 import awsconfig from "../aws-exports";
 import { StageProvider } from "../components/StageContext";
 import {
@@ -37,13 +37,14 @@ Amplify.configure(awsconfig);
 // markup
 const IndexPage = () => {
   const [currentStage, setStage] = useState(1);
-  const [id, setId] = useState();
-  const [email, setEmail] = useState("");
+  const [id, setId] = useState("nada");
+  const [email, setEmail] = useState("nada");
   const [name, setName] = useState("there");
   const [margin, setMargin] = useState("m-20");
   const [width, height] = useWindowSize();
   const [menuAlignment, setMenuAlignment] = useState("vertical");
   const [contentAlignment, setContentAlignment] = useState("left");
+  const [defIndex, setDefIndex] = useState(2);
   // for mobile interface usage
   useEffect(() => {
     if (width <= 1080 || detectMob()) {
@@ -58,40 +59,73 @@ const IndexPage = () => {
     }
   }, [width]);
 
+  const logger = new Logger("Logger", "INFO");
+  const listener = (data) => {
+    switch (data.payload.event) {
+      case "signIn":
+        setName("there");
+        logger.info("user signed in");
+        Auth.currentUserInfo({ bypassCache: true })
+          .then((data) => {
+            console.log(data.attributes.email);
+            setEmail(data.attributes.email);
+            setId("nada");
+            axios
+              .get(
+                `https://gci-backend.herokuapp.com/users?email=${data.attributes.email}`
+              )
+              .then((result) => {
+                console.log("this is the id", result.data.payload[0].id);
+                setId(result.data.payload[0].id);
+                axios
+                  .get(
+                    `https://gci-backend.herokuapp.com/users/${result.data.payload[0].id}`
+                  )
+                  .then((result) => {
+                    console.log(result.data);
+                    setStage(result.data.payload[0].current_stage);
+                    setName(result.data.payload[0].first_name);
+                  })
+                  .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                  });
+              })
+              .catch((err) => console.log(err));
+          })
+          .catch(function (error) {
+            // handle error
+            setEmail("nada");
+            console.log(error);
+            console.log(email, " ", id); // this is where you check
+          });
+        break;
+      case "signUp":
+        logger.info("user signed up");
+        break;
+      case "signOut":
+        logger.info("user signed out");
+        setEmail("nada");
+        setName("there");
+        setId("nada");
+        setStage(1);
+        break;
+      case "signIn_failure":
+        logger.info("user sign in failed");
+        break;
+      case "configured":
+        logger.info("the Auth module is configured");
+        break;
+      default:
+        logger.error("Something went wrong, look at data object", data);
+    }
+  };
+  Hub.listen("auth", listener);
+
   //this useEffect grabs the logged-in user's email from AMS Auth.
   // it uses this to identify them in the DB and fetch their user id
   // we then use this to make a GET request to find their current stage
   //this is then used to determine which stage is displayed to them
-
-  Auth.currentAuthenticatedUser({ bypassCache: true }).then((data) => {
-    setEmail(data.username);
-  });
-
-  useEffect(() => {
-    axios
-      .get(`https://gci-backend.herokuapp.com/users?email=${email}`)
-      .then((result) => {
-        if (result.data.payload[0]) {
-          console.log("this is the id", result.data.payload[0].id);
-          setId(result.data.payload[0].id);
-
-          axios
-            .get(
-              `https://gci-backend.herokuapp.com/users/${result.data.payload[0].id}`
-            )
-            .then((result) => {
-              console.log(result.data);
-              setStage(result.data.payload[0].current_stage);
-              setName(result.data.payload[0].first_name);
-            })
-            .catch(function (error) {
-              // handle error
-              console.log(error);
-            });
-        } else return;
-      })
-      .catch((err) => console.log(err));
-  }, [email]);
 
   return (
     <ChakraProvider>
@@ -118,6 +152,7 @@ const IndexPage = () => {
           <Header className="bg-gcinavy text-white" />
           <main className="wrapper">
             <Tabs
+              defaultIndex={defIndex}
               className={margin}
               variant="line"
               align={contentAlignment}
