68edba9fb08e2af0a0491fdfa209ca1577b3d448
Emanuel-David Bratasanu <davidthearchetype@gmail.com>
2021-11-05 17:41:22 +0000

    Rendering the correct stuff on result lol

--- src/components/Result/index.js
+++ src/components/Result/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Heading } from "@chakra-ui/react";
-import { Text } from "@chakra-ui/layout";
+import Interweave from "interweave";
 const axios = require("axios").default;
 
 export default function Result({ userId }) {
@@ -15,8 +15,7 @@ export default function Result({ userId }) {
 
   return (
     <section className="m-5">
-      <Heading className="text-4xl font-bold mb-5">Result</Heading>
-      <Text>{result}</Text>
+      <Interweave content={result} />
     </section>
   );
 }
