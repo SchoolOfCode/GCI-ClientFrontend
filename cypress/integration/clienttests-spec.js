8082e0177ab0fc82b25bc30c479a52b661e70cc1
Tommosaurus <87076987+Tommosaurus@users.noreply.github.com>
2021-11-08 11:11:59 +0000

    new branch for cypress testing

---
+++ cypress/integration/clienttests-spec.js
@@ -0,0 +1,53 @@
+describe("FAQ test", () => {
+  before(() => {
+    cy.signIn();
+  });
+
+  after(() => {
+    cy.clearLocalStorageSnapshot();
+    cy.clearLocalStorage();
+  });
+
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorage();
+  });
+
+  it("should log in, click on FAQ and test that one of our FAQs is visible", () => {
+    cy.visit("http://localhost:8000/");
+
+    cy.contains("Welcome").should("be.visible");
+    cy.contains("F.A.Q.").click()
+    cy.contains("love").should("be.visible")
+  });
+});
+
+describe("Result test", () => {
+    before(() => {
+      cy.signIn();
+    });
+  
+    after(() => {
+      cy.clearLocalStorageSnapshot();
+      cy.clearLocalStorage();
+    });
+  
+    beforeEach(() => {
+      cy.restoreLocalStorage();
+    });
+  
+    afterEach(() => {
+      cy.saveLocalStorage();
+    });
+  
+    it("should log in, click on Result and see a positive result for an applicant who has been accepted", () => {
+      cy.visit("http://localhost:8000/");
+  
+     
+      cy.contains("Result").click()
+      cy.contains("bootcamper").should("be.visible")
+    });
+  });
